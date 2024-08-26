4a0c1ce408c941fd044c63860efcc32c1aa0092d
Renga <157552976+ArgeiaArgia@users.noreply.github.com>
2024-08-26 19:53:26 +0900

    Title: 동전이 있는 위치, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240826/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int m;
+	cin >> n >> m;
+	int** field = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		field[i] = new int[n];
+		for (int j = 0; j < n; j++) {
+			field[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		int r, c;
+		cin >> r >> c;
+		field[r][c] = 1;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << field[i][j];
+		}
+	}
+
+	return 0;
+}
