68d60144c6713bdcfd72e2e7a0e65d8361c9ee6d
Renga <157552976+ArgeiaArgia@users.noreply.github.com>
2024-08-27 20:06:18 +0900

    Title: 재귀 속의 합계, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/재귀 속의 합계/sum-in-recursive-state.cpp
+++ 240827/재귀 속의 합계/sum-in-recursive-state.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int Sum(int num, int sum) {
-	if (num == 0) return sum;
-	sum += num;
-	Sum(num - 1, sum);
+int Sum(int num) {
+	if (num <= 0) return 0;
+	return num + Sum(num - 1);
 }
 int main() {
 	int a = 0;
-	int sum = 0;
 	cin >> a;
-	int b = Sum(a, sum);
-	cout << b;
+	cout << Sum(a);
 	return 0;
 }
