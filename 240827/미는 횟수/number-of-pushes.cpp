e925323a6515f907c69c6516498c0e7227c235d3
Renga <157552976+ArgeiaArgia@users.noreply.github.com>
2024-08-27 19:06:32 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240827/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str1, str2;
+	cin >> str1 >> str2;
+	int count = 0;
+	while (count <= str1.size()) {
+		char first = str1[0];
+		for (int i = 0; i < str1.size() - 1; i++) {
+			str1[i] = str1[i + 1];
+		}
+		str1[str1.size() - 1] = first;
+		count++;
+		if (str1 == str2) break;
+	}
+	if (count > str1.size()) {
+		count = -1;
+	}
+	cout << count;
+	return 0;
+}
